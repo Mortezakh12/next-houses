e5e33bda20d9ea6cd99bfead0921f9cad2c74842
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2023-12-29 20:16:55 +0330

    Remove Next.js template files and code.

---
+++ pages/homes/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleHomePage = () => {
+  return (
+    <div>Single Home Page</div>
+  )
+}
+
+export default SingleHomePage
---
+++ pages/homes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>All home page</div>
+  )
+}
+
+export default index
